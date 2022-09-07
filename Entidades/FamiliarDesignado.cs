d23f76fcc4bb2e5e631adbf2f4a846407c10f267
stebanqnota <stebanqnota@hotmail.com>
2022-09-07 02:48:44 -0500

    Version ocho - con todas las entidades

---
+++ Entidades/FamiliarDesignado.cs
@@ -0,0 +1,11 @@
+using System;
+namespace HospiEnCasa.App.Dominio
+{
+    public class FamiliarDesignado
+    {
+        public int Id  {get; set;}
+        public string Nombre {get; set;}
+        public string Apellidos {get; set;}
+        public string NumeroTelefono {get; set;}  
+    }
+}
